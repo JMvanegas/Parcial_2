f49d3bff95b7069ce4b19d7d2d1e108d8ca09b7f
JMvanegas <48739748+JMvanegas@users.noreply.github.com>
2019-03-19 20:02:19 -0600

    Add files via upload

---
+++ Parcial/ClassLibrary1/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial
+{
+    public class Solucion
+    {        
+        int num1, num2, total;
+        string nombre;
+        public void METODO_1()
+        {
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            if (num1 == 0)
+            {
+                Console.WriteLine("El producto de 0 por cualquier numero es 0");
+            }
+            else
+            {
+                Console.WriteLine("Ingrese el segundo numero: ");
+                num2 = int.Parse(Console.ReadLine());
+            }
+
+            total = num1 * num2;
+            Console.WriteLine("el total es; " + total);
+
+            Console.ReadKey();
+        }
+        
+        
+
+        public void METODO_2()
+        {
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir entre 0");
+            }
+            else
+            {
+                total = num1 / num2;
+            }
+            Console.WriteLine("el total es; " + total);
+            Console.ReadKey();
+        }
+
+        public void METODO_3()
+        {
+            int f;
+            for (f = 1; f == 5; f++)
+            {
+                Console.WriteLine("Ingrese su nombre: ");
+                nombre = Console.ReadLine();
+            }
+
+            Console.WriteLine("Los Estudiantes son: " + nombre);
+
+            Console.ReadKey();
+        }
+        }
+
+    }
+
+
+namespace ClassLibrary1
+{
+    public class Solucion
+    {
+    }
+}
---
+++ Parcial/Parcial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace Parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solucion obj = new Solucion();
+            obj.METODO_1();
+            //obj.METODO_2();
+            //obj.METODO_3();
+        }
+    }
+}
